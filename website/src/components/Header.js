e245004368e048dc20f7dd04f8b308b62ce442be
Ahraar Alam <ahraralam2012@gmail.com>
2019-10-20 22:49:12 +0530

    Navigation Header added

--- website/src/components/Header.js
+++ website/src/components/Header.js
@@ -3,14 +3,19 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
   <nav>
-    <ul>
-      <li>
-        <Link to={"/site"}>College</Link>
-      </li>
-      <li>
-        <Link to={"/about"}>About Us</Link>
-      </li>
-    </ul>
+    <div className="nav-wrapper">
+      <Link to={"/"} className="brand-logo center">
+        GEC BOT
+      </Link>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <Link to={"/site"}>College</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About Us</Link>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
