81a56938a74ebc43c9e51ac01a184c285504e8ef
Ahraar Alam <ahraralam2012@gmail.com>
2019-11-16 14:05:52 +0530

    chatBox done

--- website/src/components/chatbot/Chatbot.js
+++ website/src/components/chatbot/Chatbot.js
@@ -1,9 +1,171 @@
-import React from "react";
+import React, { Component } from "react";
+import Message from "./Messages";
 
-const Chatbot = () => (
-  <div>
-    <h2>Chatbot will be here</h2>
-  </div>
-);
+class Chatbot extends Component {
+
+  messagesEnd;
+    talkInput;
+
+  constructor(props) {
+    super(props);
+    // This binding is necessary to make `this` work in the callback
+    this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+
+    this.hide = this.hide.bind(this);
+    this.show = this.show.bind(this);
+    this.state = {
+      messages: [],
+      showBot: true
+    };
+  }
+
+  _handleInputKeyPress(e) {
+    if (e.key === "Enter") {
+      this.df_text_query(e.target.value);
+      e.target.value = "";
+    }
+  }
+  renderMessages(returnedMessages) {
+    if (returnedMessages) {
+      return returnedMessages.map((message, i) => {
+        return (
+          <Message
+            key={i}
+            speaks={message.speaks}
+            text={message.msg.text.text}
+          />
+        );
+      });
+    } else {
+      return null;
+    }
+  }
+
+  componentDidUpdate() {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    if (this.talkInput) {
+      this.talkInput.focus();
+    }
+  }
+
+  show(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setState({ showBot: true });
+  }
+
+hide(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setState({ showBot: false });
+  }
+
+  render() {
+    if (this.state.showBot) {
+      return (
+        <div
+          style={{
+            minHeight: 500,
+            maxHeight: 500,
+            width: 400,
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            border: "1px solid lightgray"
+          }}
+        >
+          <nav>
+            <div style={{
+              paddingLeft:5
+            }} className="nav-wrapper">
+              <a href="/" className="brand-logo">
+                ChatBot
+              </a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li>
+                  <a href="/" onClick={this.hide}>
+                    Close
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div
+            id="chatbot"
+            style={{
+              minHeight: 388,
+              maxHeight: 388,
+              width: "100%",
+              overflow: "auto"
+            }}
+          >
+            {this.renderMessages(this.state.messages)}
+            <div
+              ref={el => {
+                this.messagesEnd = el;
+              }}
+              style={{ float: "left", clear: "both" }}
+            ></div>
+          </div>
+          <div className=" col s12">
+            <input
+              style={{
+                margin: 0,
+                paddingLeft: "1%",
+                paddingRight: "1%",
+                width: "98%"
+              }}
+              ref={input => {
+                this.talkInput = input;
+              }}
+              placeholder="type a message:"
+              onKeyPress={this._handleInputKeyPress}
+              id="user_says"
+              type="text"
+            />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          style={{
+            minHeight: 30,
+            maxHeight: 500,
+            width: 400,
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            border: "1px solid lightgray"
+          }}
+        >
+          <nav>
+            <div style={{
+              paddingLeft:5
+      
+            }} className="nav-wrapper">
+              <a href="/" className="brand-logo">
+                ChatBot
+              </a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li>
+                  <a href="/" onClick={this.show}>
+                    Show
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div
+            ref={el => {
+              this.messagesEnd = el;
+            }}
+            style={{ float: "left", clear: "both" }}
+          ></div>
+        </div>
+      );
+    }
+  }
+}
 
 export default Chatbot;
--- website/src/components/pages/About.js
+++ website/src/components/pages/About.js
@@ -1,8 +1,50 @@
 import React from "react";
-
+import { Grid, Cell } from 'react-mdl';
 const About = () => (
   <div>
-    <h1>About Us</h1>
+    <Grid>
+      <Cell col={6}>
+      <div className="row">
+        <div style={{padding:'2em'}} className="col s12 m6" >
+          <div className="card">
+            <div className="card-image">
+              <img src="https://i.imgur.com/Egj2cwD.jpg" alt="avatar"
+                style={{height: '400px'}}/>
+              <span className="card-title">Ahraar Alam</span>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+              <a href="/">Profile</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </Cell>
+
+      <Cell className="resume-right-col" col={6}>
+      <div  className="row">
+        <div style={{padding:'2em'}} className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img src="https://i.imgur.com/Egj2cwD.jpg" alt="avatar"
+                style={{height: '400px'}}/>
+              <span className="card-title">Ankush Rawat</span>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+              <a href="/">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </Cell>
+    </Grid>
   </div>
 );
 
